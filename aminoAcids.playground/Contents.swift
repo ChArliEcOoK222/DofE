143d9628c7bb41f91d46a72357be05a457693290
Charlie C <charlie2007.cook@gmail.com>
2022-02-23 16:28:14 +0000

    Initial commit

---
+++ aminoAcids.playground/Contents.swift
@@ -0,0 +1,51 @@
+var codon = "UUU"
+
+// Checking for each amino acid
+
+switch codon {
+case "UUU", "UUC":
+    print("Phenylalanine")
+case "UUA", "UUG", "CUU", "CUC", "CUA":
+    print("Leucine")
+case "AUU", "AUC", "AUA":
+    print("Isoleucine")
+case "AUG":
+    print("Methionine")
+case "GUU", "GUC", "GUA", "GUG":
+    print("Valine")
+case "UCU", "UCC", "UCA", "UCG", "AGU", "AGC":
+    print("Serine")
+case "CCU", "CCC", "CCA", "CCG":
+    print("Proline")
+case "ACU", "ACC", "ACA", "ACG":
+    print("Threonine")
+case "GCU", "GCC", "GCA", "GCG":
+    print("Alanine")
+case "UAU", "UAC":
+    print("Tyrosine")
+case "UAA", "UAG", "UGA":
+    print("STOP")
+case "CAU", "CAC":
+    print("Histidine")
+case "CAA", "CAG":
+    print("Glutamine")
+case "AAU", "AAC":
+    print("Asparagine")
+case "AAA", "AAG":
+    print("Lysine")
+case "GAU", "GAC":
+    print("Aspartic Acid")
+case "GAA", "GAG":
+    print("Glutamic Acid")
+case "UGU", "UGC":
+    print("Cysteine")
+case "UGG":
+    print("Tryptophan")
+case "CGU", "CGC", "CGA", "CGG", "AGA", "AGG":
+    print("Arginine")
+case "GGU", "GGC", "GGA", "GGG":
+    print("Glycine")
+// if the codon is not recognised
+default:
+    print("Not a valid codon")
+}
