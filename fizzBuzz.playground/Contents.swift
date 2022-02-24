fd7560815af446fed95e28046570d6fb88264222
Charlie C <charlie2007.cook@gmail.com>
2022-02-24 15:17:46 +0000

    Add detailed comments for better readability

--- fizzBuzz.playground/Contents.swift
+++ fizzBuzz.playground/Contents.swift
@@ -1,10 +1,15 @@
+// looping through number range
 for num in 1...100000 {
+// checking if they are both multiples of three and five
   if num % 3 == 0 && num % 5 == 0 {
     print("FizzBuzz")
+    // checking if they are only multiples of three
   } else if num % 3 == 0 {
     print("Fizz")
+    // checking if they are only multiples of five
   } else if num % 5 == 0 {
     print("Buzz")
+    // if they are multiples of neither
   } else {
     print(num)
   }
