571c8b412edd2654fcaa894ecee39b19ec33102c
Charlie C <charlie2007.cook@gmail.com>
2022-02-24 15:40:32 +0000

    Add detailed comments for better readability

--- whaleTalk.playground/Contents.swift
+++ whaleTalk.playground/Contents.swift
@@ -1,9 +1,13 @@
-var input = "hello my name is charlie"
-
+// input to convert to whale talk
+var input = "INPUT"
+// output to store the input converted to whale talk
 var output = ""
-for char in input {
-  let lowerChar = char.lowercased()
 
+// iterating over input variable
+for char in input {
+  // constant variable to store the placeholder in lowercase
+  let lowerChar = char.lowercased()
+  // switch statement to convert to whale talk
   switch lowerChar {
   case "a", "i", "o":
     output += lowerChar.uppercased()
@@ -11,9 +15,11 @@ for char in input {
     output += "EE"
   case "u":
     output += "UU"
+  // default just leaves the letter
   default:
     continue
   }
 }
 
+// printing the final ouput in whale talk
 print(output)
