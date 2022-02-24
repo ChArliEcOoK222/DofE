1cca5986f95be8f805505eb4610d42fa47e09c38
Charlie C <charlie2007.cook@gmail.com>
2022-02-24 15:37:10 +0000

    Add detailed comments for better readability

--- palindrome.playground/Contents.swift
+++ palindrome.playground/Contents.swift
@@ -1,14 +1,21 @@
+// array to store characters in the same order as word
 var text: [String] = ["l", "e", "v", "e", "l"]
+// array to store the text array but in reversed order
 var reversed = [String]()
+// while loop counter
 var counter = text.count - 1
 
+// while loop to reverse to text array and append each character to the reverse array
 while counter >= 0 {
   reversed.append(text[counter])
   counter -= 1
 }
 
+// checking if the arrays are equal
 if text == reversed {
+  // if they are equal, this is printed
   print("We have a palindrome!")
 } else {
+  // if they are not, this is printed
   print("We don't have a palindrome")
 }
